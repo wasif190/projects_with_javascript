a7502f970e3145c9c80f6bfa9b0bcb9dbc539aa4
Wasif <137194499+wasif190@users.noreply.github.com>
2025-11-29 08:56:37 +0000

    local storage, delete

--- 01_todo_app/script.js
+++ 01_todo_app/script.js
@@ -1,22 +1,59 @@
-const todoInput = document.querySelector("#todo-input")
-const addtaskBtn = document.querySelector("#add-task-btn")
-const todoLists = document.querySelector("#todo-list")
+document.addEventListener('DOMContentLoaded', () => {
+    const todoInput = document.querySelector("#todo-input")
+    const addtaskBtn = document.querySelector("#add-task-btn")
+    const todoLists = document.querySelector("#todo-list")
 
-let tasks = []
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-function addTask(e) {
-    const taskText = todoInput.value.trim()
-    if (taskText == "") alert("Please enter something");
+    tasks.forEach(task => renderTask(task));
 
-    const newTask = {
-        id: Date.now(),
-        text: taskText,
-        completed: false
+    function addTask() {
+        const taskText = todoInput.value.trim()
+        if (taskText == "") return;
+
+        const newTask = {
+            id: Date.now(),
+            text: taskText,
+            completed: false
+        }
+
+        tasks.push(newTask);
+        saveTasks();
+        todoInput.value = "";
+        console.log(tasks)
     }
 
-    tasks.push(newTask);
-    todoInput.value = "";
-    console.log(tasks)
-}
+    function renderTask(task) {
+        const li = document.createElement('li')
+        li.setAttribute('data-id', task.id)
 
-addtaskBtn.addEventListener('click', addTask)
+        if (task.complited) li.classList.add('complited')
+        li.innerHTML = `
+            <span>${task.text}</span>
+            <button>delete</button>
+        `;
+
+        li.addEventListener('click', (e) => {
+            if(e.target.tagName == 'BUTTON') return;
+            task.complited = !task.completed;
+            li.classList.toggle('complited')
+            saveTasks()
+        });
+
+        li.querySelector('button').addEventListener('click', (e) => {
+            e.stopPropagation() // prevent toggle from firing
+            tasks = tasks.filter(t => t.id !== task.id)
+            li.remove();
+            saveTasks()
+        })
+
+        todoLists.appendChild(li);
+    }
+
+    function saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+
+
+    addtaskBtn.addEventListener('click', addTask)
+})
